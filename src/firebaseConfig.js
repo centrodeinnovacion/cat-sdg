ef33432d22692793189085ed360575e3880c50be
Juan Carlos Giraldo Cardozo <jgiraldocardozo@gmail.com>
2018-12-28 15:11:15 -0500

    Ajustes a la configuración de la base de datos en Firebase
    Utilice los datos de su proyecto firebase para aplicaciones Web.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,19 +1,8 @@
-// // CAT-SDG con jgiraldocardozo@gmail.com
-// export default {
-//   apiKey: 'AIzaSyDr0-Mef6D1RZsD2NoBaPOwordhUW58MyU',
-//   authDomain: 'contacts-app-dca62.firebaseapp.com',
-//   databaseURL: 'https://contacts-app-dca62.firebaseio.com',
-//   projectId: 'contacts-app-dca62',
-//   storageBucket: 'contacts-app-dca62.appspot.com',
-//   messagingSenderId: '715354469790'
-// }
-
-// CAT-SDG con jgiraldo@correo.unicordoba.edu.co
 export default {
-  apiKey: 'AIzaSyBrmU6VDuZf_q2kpQEx498C6om3LHS8zyY',
-  authDomain: 'cat-sdg-aa433.firebaseapp.com',
-  databaseURL: 'https://cat-sdg-aa433.firebaseio.com',
-  projectId: 'cat-sdg-aa433',
-  storageBucket: 'cat-sdg-aa433.appspot.com',
-  messagingSenderId: '24774196594'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 }
